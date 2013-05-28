0a3c40613f427ee7f641b623fe58b9b0a90aca78
Eduardo Suarez-Santana <eduardo@suarezsantana.com>
2013-05-28 19:07:35 +0100

    Plugin file added.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 yourls-edition-logger
 =====================
 
-yourls-edition-logger
+This [yourls](http://yourls.org/) plugin logs to a file every url insertion, deletion, or modification. So that, it provides traceability of users' actions allowing an open edition policy.
+
+
+Installation
+------------
+
+ * Follow [yourls guidelines](https://github.com/YOURLS/YOURLS/wiki) to install a plugin.
+ * Download [KLogger](https://github.com/katzgrau/KLogger).
+ * Setup `EDITIONLOGGER_KLOGGER_PATH` and `EDITIONLOGGER_LOGFILE` in `user/config.php`.
+
+For more information, follow me at [http://e.suarezsantana.com/](http://e.suarezsantana.com/).
+
---
+++ plugin.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Plugin Name: Edition Logger
+Plugin URI: https://github.com/esuarezsantana/yourls-edition-logger
+Description: Log every link edition
+Version: 1.0
+Author: Eduardo Suarez-Santana
+Author URI: http://e.suarezsantana.com/
+*/
+
+// No direct call
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+require_once EDITIONLOGGER_KLOGGER_PATH . '/src/KLogger.php';
+
+function editionlogger_environment_check() {
+    $required_params = array(
+        'EDITIONLOGGER_KLOGGER_PATH', // path to KLogger
+        'EDITIONLOGGER_LOGFILE', // File to log
+    );
+
+    foreach ($required_params as $pname) {
+        if ( !defined( $pname ) ) {
+            $message = 'Missing defined parameter '.$pname.' in plugin '. $thisplugname;
+            error_log($message);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function editionlogger_insert_link ($args) {
+        editionlogger_environment_check();
+        $insert = $args[0];
+    $url = $args[1];
+    $keyword = $args[2];
+        if ( $insert ) {
+            $log = new KLogger ( EDITIONLOGGER_LOGFILE , KLogger::DEBUG );
+            $log->LogInfo('['.YOURLS_USER."] Link inserted: ( $keyword, $url )");
+        }
+}
+
+function editionlogger_delete_link ($args) {
+        editionlogger_environment_check();
+    $keyword = $args[0];
+        $log = new KLogger ( EDITIONLOGGER_LOGFILE , KLogger::DEBUG );
+        $log->LogInfo('['.YOURLS_USER."] Link deleted: ( $keyword )");
+}
+
+function editionlogger_edit_link ($args) {
+        editionlogger_environment_check();
+    $url = $args[0];
+    $keyword = $args[1];
+    $newkeyword = $args[2];
+    $new_url_already_there = $args[3];
+    $keyword_is_ok = $args[4];
+        // same check as in the source
+        if ( ( !$new_url_already_there || yourls_allow_duplicate_longurls() ) && $keyword_is_ok ) {
+            $log = new KLogger ( EDITIONLOGGER_LOGFILE , KLogger::DEBUG );
+            $log->LogInfo('['.YOURLS_USER."] Link edited: $keyword -> ( $newkeyword, $url )");
+        }
+}
+
+yourls_add_action( 'insert_link',   'editionlogger_insert_link' );
+yourls_add_action( 'delete_link',   'editionlogger_delete_link' );
+yourls_add_action( 'pre_edit_link', 'editionlogger_edit_link' );
+
